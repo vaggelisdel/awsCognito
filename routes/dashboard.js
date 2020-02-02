018662b899abb45d39f57e21733c45a01a32af53
vaggelisdel <delliosvaggelis@gmail.com>
2020-02-02 16:01:12 +0200

    sdfsdf

---
+++ routes/dashboard.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+require('cross-fetch/polyfill');
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const request = require('request');
+var sess;
+
+CognitoExpress = require("cognito-express");
+const app = express();
+app.use("/", router);
+const cognitoExpress = new CognitoExpress({
+    region: "us-east-2",
+    cognitoUserPoolId: "us-east-2_1PKgjCHvs",
+    tokenUse: "access", //Possible Values: access | id
+    tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+});
+
+router.use(function (req, res, next) {
+    sess = req.session;
+    //I'm passing in the access token in header under key accessToken
+    let accessTokenFromClient = req.headers.accesstoken || sess.accesstoken;
+    //Fail if token not present in header.
+
+    // if (!accessTokenFromClient) return res.status(401).send("Access Token missing from header");
+    if (!accessTokenFromClient) return res.redirect('/signin');
+
+    cognitoExpress.validate(accessTokenFromClient, function (err, response) {
+
+        //If API is not authenticated, Return 401 with error message.
+        if (err) return res.status(401).send(err);
+
+        //Else API has been authenticated. Proceed.
+        res.locals.user = response;
+        next();
+    });
+});
+
+router.get('/', function (req, res, next) {
+    // const options = {
+    //     url: 'https://0nerp7erwl.execute-api.us-east-2.amazonaws.com/dev',
+    //     method: 'GET',
+    //     headers: {
+    //         'Authorization': req.headers.accesstoken || sess.accesstoken
+    //     }
+    // };
+    //
+    // request(options, function(err, response, body) {
+    //     let json = JSON.parse(body);
+    //     res.send(json);
+    // });
+    res.send(`Hi ${res.locals.user.username}, your API call is authenticated!`);
+});
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+require('cross-fetch/polyfill');
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+var sess;
+
+
+var poolData = {
+    UserPoolId: 'us-east-2_1PKgjCHvs', // Your user pool id here
+    ClientId: '2jnef8dug84shaot8grvdg45lg', // Your client id here
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Signup'});
+});
+router.post('/', function (req, res, next) {
+    var attributeList = [];
+
+    var dataEmail = {
+        Name: 'email',
+        Value: req.body.email,
+    };
+
+    var dataPhoneNumber = {
+        Name: 'phone_number',
+        Value: req.body.phone_number,
+    };
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+    var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(
+        dataPhoneNumber
+    );
+
+    attributeList.push(attributeEmail);
+    attributeList.push(attributePhoneNumber);
+
+    userPool.signUp(req.body.username, req.body.password, attributeList, null, function (err, result) {
+        if (err) {
+            console.log(err.message || JSON.stringify(err));
+            return;
+        }
+        var cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+        res.redirect('/signin');
+    });
+});
+
+router.get('/signin', function (req, res, next) {
+    res.render('signin', {title: 'Signin'});
+});
+router.post('/signin', function (req, res, next) {
+    sess = req.session;
+
+    var authenticationData = {
+        // Username: req.body.username,
+        // Password: req.body.password,
+        Username: 'vaggelisdel',
+        Password: 'Del12345!@#$%',
+    };
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+        authenticationData
+    );
+
+    var userData = {
+        // Username: req.body.username,
+        Username: 'vaggelisdel',
+        Pool: userPool,
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            sess.accesstoken = result.getAccessToken().getJwtToken();
+            // console.log(result.getIdToken().getJwtToken());
+            console.log("Connected!!");
+            res.redirect('/dashboard');
+        },
+        onFailure: function (err) {
+            console.log(err.message || JSON.stringify(err));
+            res.redirect('/signin');
+        },
+    });
+});
+
+module.exports = router;
